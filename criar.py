ed2129744b7fffe48ac5023604751f127723b7e5
StephaniePlatvoet <142596556+StephaniePlatvoet@users.noreply.github.com>
2024-05-24 20:39:19 +0100

    Update criar.py

--- criar.py
+++ criar.py
@@ -4,11 +4,13 @@ from dateutil.rrule import SU
 from funcoes import calcular_proxima_data, criarImport
 
 def criar(task_class,db):
+
+    data_primeira_vez_str = datetime.strptime(request.form['data_primeira_vez']
     
     nova_tarefa = task_class(
     descricao=request.form['descricao_tarefa'],
     feita=False,
-    data_primeira_vez=datetime.strptime(request.form['data_primeira_vez'], '%Y-%m-%d') if data_primeira_vez_str else datetime.now().replace(hour=0, minute=0, second=0, microsecond=0),
+    data_primeira_vez=data_primeira_vez_str, '%Y-%m-%d') if data_primeira_vez_str else datetime.now().replace(hour=0, minute=0, second=0, microsecond=0),
     intervalo_repeticao_mode=request.form.get('intervalo_repeticao_mode'),
     intervalo_repeticao_value=request.form.get('intervalo_repeticao_value'),
     proximo_domingo=bool(request.form.get('intervalo_repeticao_next_sunday')),
