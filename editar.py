dc77d6ec889e95bda675866cef12ca164ae5856d
StephaniePlatvoet <142596556+StephaniePlatvoet@users.noreply.github.com>
2024-05-25 17:07:10 +0100

    Update editar.py

--- editar.py
+++ editar.py
@@ -1,11 +1,12 @@
 from flask import request, url_for, redirect, flash
 from datetime import datetime
+from funcoes import custom_order
 
 def editar(Tarefa, db, id):
     tarefa = Tarefa.query.get_or_404(id)
     if tarefa:
         # Lista de campos a serem atualizados
-        campos = ['descricao', 'feita', 'notas', 'classe', 'ordem', 'data_primeira_vez', 'data_proxima', 'data_proxima_seguinte', 'intervalo_repeticao_mode', 'intervalo_repeticao_value', 'proximo_domingo', 'owner']
+        campos = ['descricao', 'feita', 'notas', 'classe', 'data_primeira_vez', 'data_proxima', 'data_proxima_seguinte', 'intervalo_repeticao_mode', 'intervalo_repeticao_value', 'proximo_domingo', 'owner']
         
         # Mapeamento de campos booleanos e datetime
         campos_booleanos = ['feita', 'proximo_domingo']
@@ -27,17 +28,22 @@ def editar(Tarefa, db, id):
                 else:
                     flash(f'Campo {campo} não pode estar vazio.', 'error')
                     erro = True
-            
-            # Adicione outras validações conforme necessário
+
             if campo not in campos_booleanos and campo not in campos_datetime:
-                if not valor_formulario:
+                if not valor_formulario and campo != 'ordem':
                     flash(f'Campo {campo} não pode estar vazio.', 'error')
                     erro = True
 
             valor_atual = getattr(tarefa, campo)
             if not erro and valor_formulario != valor_atual:
                 setattr(tarefa, campo, valor_formulario)
-        
+
+        # Tratamento específico para o campo 'ordem'
+        ordem = request.form.get('ordem')
+        ordem = custom_order(ordem)  # Usando a função custom_order para tratar o valor
+        if not erro and ordem != getattr(tarefa, 'ordem'):
+            setattr(tarefa, 'ordem', ordem)
+
         if erro:
             return redirect(url_for('editar_tarefa', id=id))
 
