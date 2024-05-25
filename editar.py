c959cdef88f25e1ecf40624a10c4a68c3ae37232
StephaniePlatvoet <142596556+StephaniePlatvoet@users.noreply.github.com>
2024-05-25 16:50:24 +0100

    Update editar.py

--- editar.py
+++ editar.py
@@ -1,18 +1,32 @@
-from flask import request, url_for,redirect,flash
+from flask import request, url_for, redirect, flash
 from datetime import datetime
 
-
-def editar(Tarefa,db,id):
+def editar(Tarefa, db, id):
     tarefa = Tarefa.query.get_or_404(id)
     if tarefa:
         # Atualizar os campos da tarefa com os valores recebidos do formulário
-        tarefa.descricao = request.form['descricao']
+        tarefa.descricao = request.form.get('descricao')
         tarefa.feita = 'feita' in request.form
-        tarefa.notas = request.form['notas']
-        tarefa.classe = request.form['classe']
+        tarefa.notas = request.form.get('notas')
+        tarefa.classe = request.form.get('classe')
         tarefa.ordem = request.form.get('ordem', type=int)
         tarefa.data_alteracao = datetime.utcnow()
-        # Adicione ou atualize outros campos conforme necessário
+
+        # Atualizar campos adicionais conforme necessário
+        intervalo_repeticao_mode = request.form.get('intervalo_repeticao_mode')
+        intervalo_repeticao_value = request.form.get('intervalo_repeticao_value')
+        intervalo_repeticao_next_sunday = request.form.get('intervalo_repeticao_next_sunday')
+        data_primeira_vez_str = request.form.get('data_primeira_vez')
+        data_primeira_vez = datetime.strptime(data_primeira_vez_str, '%Y-%m-%d') if data_primeira_vez_str else datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+        tarefa.intervalo_repeticao_mode = intervalo_repeticao_mode
+        tarefa.intervalo_repeticao_value = intervalo_repeticao_value
+        tarefa.proximo_domingo = bool(intervalo_repeticao_next_sunday)
+        tarefa.data_primeira_vez = data_primeira_vez
+
+        # Recalcular as datas de repetição, se necessário
+        tarefa.data_proxima = calcular_proxima_data(data_primeira_vez, intervalo_repeticao_next_sunday, intervalo_repeticao_value, intervalo_repeticao_mode, primeiraVez=True)
+        tarefa.data_proxima_seguinte = calcular_proxima_data(data_primeira_vez, intervalo_repeticao_next_sunday, intervalo_repeticao_value, intervalo_repeticao_mode, primeiraVez=False)
 
         try:
             db.session.commit()
