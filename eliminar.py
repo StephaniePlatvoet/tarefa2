70bb3735cc7e19b811ef717cf81f258db7e38b4d
StephaniePlatvoet <142596556+StephaniePlatvoet@users.noreply.github.com>
2024-06-22 12:28:04 +0100

    Update eliminar.py

--- eliminar.py
+++ eliminar.py
@@ -4,4 +4,4 @@ from flask import redirect, url_for
 def eliminar(task_class,db,id):
     task_class.query.filter_by(id=int(id)).delete()
     db.session.commit()
-    return redirect(url_for('go_home') + '#content')
+    return redirect(url_for('do_filter') + '#content')
